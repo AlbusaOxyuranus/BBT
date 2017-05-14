1d7534a8ddc73a2d1aca1ea21743cf4a09556842
Denis Prokhorchik <Dionis_999@hotmail.com>
2017-05-14 23:20:19 +0300

    add ObservableObject class

---
+++ scr/BlackBee.Toolkit/BlackBee.Toolkit.Base/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿namespace BlackBee.Toolkit.Base
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
