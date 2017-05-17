e448381dd974f2bbd05356bf48b5eb3250f04c40
Denis Prokhorchik <Dionis_999@hotmail.com>
2017-05-17 23:39:07 +0300

    fix version 0-0-7

--- scr/BlackBee.Toolkit/BlackBee.Toolkit.Base/Commands/RelayCommand.cs
+++ scr/BlackBee.Toolkit/BlackBee.Toolkit.Base/Commands/RelayCommand.cs
@@ -2,6 +2,7 @@
 using System.Windows.Input;
 
 namespace BlackBee.Toolkit.Base.Commands
+{ 
     /// <summary>
     /// A command whose sole purpose is to relay its functionality 
     /// to other objects by invoking delegates. 
