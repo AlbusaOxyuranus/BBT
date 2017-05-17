6d985dd9b31eb0cce83a28de4f2aae213765091a
Denis Prokhorchik <Dionis_999@hotmail.com>
2017-05-17 23:29:40 +0300

    fix version

--- scr/BlackBee.Toolkit/BlackBee.Toolkit.Base/ObservableObject.cs
+++ scr/BlackBee.Toolkit/BlackBee.Toolkit.Base/ObservableObject.cs
@@ -8,7 +8,7 @@
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
