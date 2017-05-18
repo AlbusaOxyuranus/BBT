26e5d7ff0e5ad32cd715c69f4be33e0c3d5bacb3
Denis Prokhorchik <Dionis_999@hotmail.com>
2017-05-19 01:13:13 +0300

    add busy lib

---
+++ scr/BlackBee.Toolkit/BlackBee.Toolkit.Base/Converters/ConvertBoolToVisibility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace BlackBee.Toolkit.Base.Converters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ConvertBoolToVisibility : IValueConverter
+    {
+        //public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        //{
+        //    if (bool.Parse(value.ToString()))
+        //        return Visibility.Visible;
+        //    return Visibility.Collapsed;
+        //}
+
+        //public object ConvertBack(object value, Type targetType, object parameter,
+        //    CultureInfo culture)
+        //{
+        //    Visibility visibility;
+        //    Visibility.TryParse(value.ToString(), true, out visibility);
+        //    if (visibility == Visibility.Visible)
+        //        return true;
+        //    return false;
+        //}
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (bool.Parse(value.ToString()))
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            Visibility visibility;
+            Visibility.TryParse(value.ToString(), true, out visibility);
+            if (visibility == Visibility.Visible)
+                return true;
+            return false;
+        }
+    }
+}
